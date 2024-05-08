eb3676a3d227bb24a01a57e80f87fbc18fa91dcb
Pēteris Paikens <peterisp@gmail.com>
2024-05-08 15:10:15 +0300

    Generation of words with non-standard lemma with pre-provided stems

--- src/test/java/lv/semti/morphology/Testi/MorphologyTest.java
+++ src/test/java/lv/semti/morphology/Testi/MorphologyTest.java
@@ -1985,7 +1985,7 @@ public class MorphologyTest {
         // Vietniekvārdiem neieliek pēdējo pozīciju tagā (noliegumu); -šana atvasinātās formas nav ok
         Word viņš = locītājs.analyze("viņš");
         assertTrue(viņš.isRecognized());
-        assertEquals("pp3msnn", viņš.getBestWordform().getTag());
+        assertEquals("pd3msnn", viņš.getBestWordform().getTag());
 
         Word ciršana = locītājs.analyze("ciršana");
         assertTrue(ciršana.isRecognized());
@@ -2911,7 +2911,7 @@ public class MorphologyTest {
     public void plāns_B() {
         Word w = locītājs.analyze("B");
         assertTrue(w.isRecognized());
-        assertEquals("xx", w.getBestWordform().getTag());
+        assertEquals("y", w.getBestWordform().getTag());
     }
 
     @Test
